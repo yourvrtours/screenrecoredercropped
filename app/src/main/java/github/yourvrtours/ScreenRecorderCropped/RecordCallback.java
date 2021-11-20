9a28b7dc0185872a05d19a09002318a1f54c6df8
yourvrtours <support@yourvrtours.com>
2021-11-20 15:02:30 +0000

    Added all files

---
+++ app/src/main/java/github/yourvrtours/ScreenRecorderCropped/RecordCallback.java
@@ -0,0 +1,25 @@
+package github.yourvrtours.ScreenRecorderCropped;
+
+/**
+ * Record callback
+ */
+public interface RecordCallback {
+
+    /**
+     * Callback when record successfully
+     * @param filePath recorded MP4 file path
+     */
+    void onRecordSuccess(String filePath, String coverPath, long duration);
+
+    /**
+     * Callback when record failed
+     * @param e reason why it failed
+     */
+    void onRecordFailed(Throwable e, long duration);
+
+    /**
+     * Record progress changed
+     * @param ms current record duration in ms
+     */
+    void onRecordedDurationChanged(long ms);
+}
